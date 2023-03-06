c33d70f8923f9e4b4331a1ad5850fbfe9bce357b
minhvvan <eminhwan12@gmail.com>
2023-03-06 21:53:11 +0900

    Initial commit
    (+)Rotate Actor

---
+++ Test/Source/Test/MyActor.cpp
@@ -0,0 +1,38 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MyActor.h"
+
+// Sets default values
+AMyActor::AMyActor()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	Mesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("MESH"));
+
+	ConstructorHelpers::FObjectFinder<UStaticMesh> SM(TEXT("/Script/Engine.StaticMesh'/Game/StarterContent/Props/SM_Couch.SM_Couch'"));
+
+	if (SM.Succeeded()) {
+		Mesh->SetStaticMesh(SM.Object);
+	}
+}
+
+// Called when the game starts or when spawned
+void AMyActor::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	//UE_LOG(LogTemp, Warning, TEXT("Begin Play %d"), 3);
+}
+
+// Called every frame
+void AMyActor::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	//UE_LOG(LogTemp, Error, TEXT("Tick %f"), DeltaTime);
+
+	AddActorLocalRotation(FRotator(0.f, RotateSpeed * DeltaTime, 0.f));
+}
+
