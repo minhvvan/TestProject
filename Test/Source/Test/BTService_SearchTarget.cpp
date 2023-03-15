0246afdcafe588159e284e351e33ef9a5d3f3c4f
minhvvan <eminhwan12@gmail.com>
2023-03-15 15:14:00 +0900

    Behavior Tree#2
    make Task & Decorator & Service

---
+++ Test/Source/Test/BTService_SearchTarget.cpp
@@ -0,0 +1,61 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "BTService_SearchTarget.h"
+#include "MyAIController.h"
+#include "MyCharacter.h"
+#include "BehaviorTree/BlackboardComponent.h"
+#include "DrawDebugHelpers.h"
+
+UBTService_SearchTarget::UBTService_SearchTarget()
+{
+	NodeName = TEXT("SearchTarget");
+	Interval = 1.f;
+}
+
+void UBTService_SearchTarget::TickNode(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory, float DeltaSeconds)
+{
+	Super::TickNode(OwnerComp, NodeMemory, DeltaSeconds);
+
+	auto CurrentPawn = OwnerComp.GetAIOwner()->GetPawn();
+	if (CurrentPawn == nullptr) return;
+
+	UWorld* World = CurrentPawn->GetWorld();
+	FVector Center = CurrentPawn->GetActorLocation();
+	float SearchRadius = 500.f;
+
+	if (World == nullptr) return;
+
+	TArray<FOverlapResult> OverlapResults;
+	FCollisionQueryParams QuertParams(NAME_None, false, CurrentPawn);
+
+	bool bResult = World->OverlapMultiByChannel(
+		OverlapResults,
+		Center,
+		FQuat::Identity,
+		ECollisionChannel::ECC_EngineTraceChannel2,
+		FCollisionShape::MakeSphere(SearchRadius),
+		QuertParams);
+
+	if (bResult)
+	{
+		for (auto& OverlapResult : OverlapResults)
+		{
+			AMyCharacter* MyCharacter = Cast<AMyCharacter>(OverlapResult.GetActor());
+			if (MyCharacter && MyCharacter->GetController()->IsPlayerController())
+			{
+				OwnerComp.GetBlackboardComponent()->SetValueAsObject(FName(TEXT("Target")), MyCharacter);
+
+				DrawDebugSphere(World, Center, SearchRadius, 16, FColor::Blue, false, 0.2f);
+				return;
+			}
+		}
+	}
+	else 
+	{
+		OwnerComp.GetBlackboardComponent()->SetValueAsObject(FName(TEXT("Target")), nullptr);
+	}
+
+	DrawDebugSphere(World, Center, SearchRadius, 16, FColor::Magenta, false, 0.2f);
+
+}
--- Test/Source/Test/MyCharacter.cpp
+++ Test/Source/Test/MyCharacter.cpp
@@ -193,6 +193,7 @@ void AMyCharacter::Yaw(float Value)
 void AMyCharacter::OnAttackMontageEnded(UAnimMontage* Montage, bool bInterrupted) 
 {
 	IsAttacking = false;
+	OnAttackEnd.Broadcast();
 }
 
 float AMyCharacter::TakeDamage(float DamageAmount, FDamageEvent const& DamageEvent, AController* EventInstigator, AActor* DamageCauser)
